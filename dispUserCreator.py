a3318a7684a72ad3b68dceb43d81fe58b980894f
11101001101000 <piapia78490@gmail.com>
2022-08-21 12:45:36 +0900

    update2

--- dispUserCreator.py
+++ dispUserCreator.py
@@ -3,5 +3,5 @@ import random
 userNum = str(random.randrange(1,2000))
 print("adduser disp" + userNum)
 os.system("sudo adduser disp" + userNum)
-os.system("su disp" + userNum)
+os.system("su -l disp" + userNum)
 os.system("sudo userdel disp" + userNum)
