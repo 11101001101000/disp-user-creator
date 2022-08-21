4c8265db8b75f95da4aa9b31ac1d40ba92f51604
11101001101000 <piapia78490@gmail.com>
2022-08-21 12:56:51 +0900

    update3

--- dispUserCreator.py
+++ dispUserCreator.py
@@ -1,8 +1,11 @@
 import os
 import random
+import time
 userNum = str(random.randrange(1,2000))
 print("adduser disp" + userNum)
 os.system("sudo adduser disp" + userNum)
+print("Switching to created disp-user. Type disp" + userNum + "\'s password.")
 os.system("su -l disp" + userNum)
 os.system("sudo pkill -9 -u disp" + userNum)
+time.sleep(0.5)
 os.system("sudo userdel disp" + userNum)
